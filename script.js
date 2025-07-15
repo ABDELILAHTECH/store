2043cd859c911c4e2946c7db3abd2b22f041f917
ABDELILAHTECH <ABDELILAHTECH1@GMAIL.COM>
2025-07-15 14:13:18 +0100

    second commit

--- script.js
+++ script.js
@@ -3,12 +3,13 @@
         let resetBtn = document.querySelector("#reset")
         let total = parseFloat(localStorage.getItem("total")) || 0;
         let panier = JSON.parse(localStorage.getItem("panier")) || [];
-        priceTotal.innerHTML = total.toFixed(2);
+        
 
-       cd class Panier {
-          constructor(img, nom, price) {
+        class Panier {
+          constructor(img, nom,counter, price) {
             this.img = img;
             this.nom = nom;
+            this.counter = counter;
             this.price = price;
         }
 }
@@ -18,26 +19,31 @@ products.forEach((product) => {
   let button = product.querySelector("button");
   let nom = product.querySelector("p");
   let priceSpan = product.querySelector("span");
+  let counter = 0;
 
   button.addEventListener("click", () => {
+
+    counter++
     let price = parseFloat(priceSpan.textContent.replace(",", "."));
-    const newPr = new Panier(img.src, nom.textContent, price);
+    const newPr = new Panier(img.src, nom.textContent,counter, price);
 
     panier.push(newPr);
     total += price;
+    console.log(total.toFixed(2));
+    
 
     localStorage.setItem("newProduct", JSON.stringify(panier));
     localStorage.setItem("total", total);
 
-    priceTotal.textContent = total.toFixed(2).replace(".", ",");
+    // priceTotal.textContent = total.toFixed(2).replace(".", ",");
   });
 });
 
 
 
 
-        resetBtn.addEventListener("click",()=>{
-            total = 0
-            localStorage.setItem("total",total)
-            priceTotal.innerHTML = total.toFixed(2);
-        })
+        // resetBtn.addEventListener("click",()=>{
+        //     total = 0
+        //     localStorage.setItem("total",total)
+        //     priceTotal.innerHTML = total.toFixed(2);
+        // })
